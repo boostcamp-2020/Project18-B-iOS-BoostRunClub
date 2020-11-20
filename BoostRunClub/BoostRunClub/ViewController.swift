e810bc6a121a77327b5c19bd7aed4dc2e25a6c0f
SHINVVOO <sinsdhk@gmail.com>
2020-11-20 11:32:40 +0900

    (#5) chore: SwiftFormat Pod 적용

--- BoostRunClub/BoostRunClub/ViewController.swift
+++ BoostRunClub/BoostRunClub/ViewController.swift
@@ -8,11 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
-
 }
