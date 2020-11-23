5f5883c7f41528581ce67949207bb536a010b263
Eric Jang <940709@gmail.com>
2020-11-23 17:46:05 +0900

    (#19) chore: Add missing files

--- BoostRunClub/Coordinators/TabBarCoordinator.swift
+++ BoostRunClub/Coordinators/TabBarCoordinator.swift
@@ -11,9 +11,9 @@ enum TabBarPage: Int {
     case activity
     case running
     case profile
-    
+
     // Add tab icon value
-    
+
     // Add tab icon selected / deselected color
 }
 
@@ -23,32 +23,32 @@ protocol MainTabBarCoordinatorProtocol: Coordinator {
 
 final class MainTabBarCoordinator: NSObject, MainTabBarCoordinatorProtocol {
     var tabBarController = UITabBarController()
-    
+
     var navigationController: UINavigationController
-    
+
     var childCoordinators = [Coordinator]()
-    
+
     init(_ navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
-    
+
     func start() {
         prepareTabBarController()
     }
-    
+
     private func prepareTabBarController() {
         childCoordinators = [
             ActivityCoordinator(UINavigationController()),
             RunningCoordinator(UINavigationController()),
             ProfileCoordinator(UINavigationController()),
         ]
-        
+
         childCoordinators.forEach { $0.start() }
-        
+
         tabBarController.setViewControllers(childCoordinators.map { $0.navigationController }, animated: true)
         tabBarController.selectedIndex = TabBarPage.running.rawValue
         tabBarController.tabBar.isTranslucent = false
-        
+
         navigationController.viewControllers = [tabBarController]
     }
 }
