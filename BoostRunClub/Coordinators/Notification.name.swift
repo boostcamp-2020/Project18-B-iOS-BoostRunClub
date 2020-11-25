4d196c6cf8e0b0b495bc038a64ce25614df24975
gihyun cho <whrlgus33@gmail.com>
2020-11-26 02:47:21 +0900

    (#8) prepareRun goalValueView 구현
    - 팩토리 패턴 적용할 예정이라 조금 구현햇음
    - prepareRunVM, completion handler -> CurrentValueSubject

---
+++ BoostRunClub/Coordinators/Notification.name.swift
@@ -0,0 +1,8 @@
+//
+//  Notification.name.swift
+//  BoostRunClub
+//
+//  Created by 조기현 on 2020/11/25.
+//
+
+import Foundation
---
+++ BoostRunClub/ViewModels/RunningViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RunningViewModel.swift
+//  BoostRunClub
+//
+//  Created by 조기현 on 2020/11/25.
+//
+
+import Foundation
