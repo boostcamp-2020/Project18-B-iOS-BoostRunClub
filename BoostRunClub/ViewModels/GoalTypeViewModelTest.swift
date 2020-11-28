f375d998292a1e98f6258607e3cb8f999f09e803
SHINVVOO <sinsdhk@gmail.com>
2020-11-29 03:57:39 +0900

    (#37) test: GoalValueSetupViewModelTest makeVM, InitialInput, FilterInput

--- BoostRunClub/ViewModels/GoalTypeViewModelTest.swift
+++ BoostRunClub/ViewModels/GoalTypeViewModelTest.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 class GoalTypeViewModelTest: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -24,9 +23,8 @@ class GoalTypeViewModelTest: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- BoostRunClub/ViewModels/GoalValueSetupViewModelTest.swift
+++ BoostRunClub/ViewModels/GoalValueSetupViewModelTest.swift
@@ -5,28 +5,183 @@
 //  Created by 조기현 on 2020/11/29.
 //
 
+@testable import BoostRunClub
+import Combine
 import XCTest
 
 class GoalValueSetupViewModelTest: XCTestCase {
+    var goalValueSetupVM: GoalValueSetupViewModel!
+    var cancellables: Set<AnyCancellable>!
+    /*
+     protocol GoalValueSetupViewModelInputs {
+         func didDeleteBackward()
+         func didInputNumber(_ number: String)
+         func didTapCancelButton()
+         func didTapApplyButton()
+     }
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+     protocol GoalValueSetupViewModelOutputs {
+         var goalValueObservable: CurrentValueSubject<String, Never> { get }
+         var runningEstimationObservable: AnyPublisher<String, Never> { get }
+         var closeSheetSignal: PassthroughSubject<String?, Never> { get }
+         var goalType: GoalType { get }
+     }
+     */
+
+    override func setUp() {}
+
+    override func tearDown() {}
+
+    func testMakeViewModel_TimeType() {
+        let expectedObserve = expectation(description: "setupWithTime")
+        let goalInfo = GoalInfo(goalType: .time, goalValue: "12:22")
+        let viewModel = GoalValueSetupViewModel(goalType: goalInfo.goalType, goalValue: goalInfo.goalValue)
+        let cancellable = viewModel.goalValueObservable
+            .first()
+            .sink { presentingValue in
+                if presentingValue == goalInfo.goalValue {
+                    expectedObserve.fulfill()
+                }
+            }
+
+        waitForExpectations(timeout: 1, handler: nil)
+        cancellable.cancel()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMakeViewModel_DistanceType() {
+        let expectedObserve = expectation(description: "setupWithDistance")
+        let goalInfo = GoalInfo(goalType: .distance, goalValue: "25")
+        let viewModel = GoalValueSetupViewModel(goalType: goalInfo.goalType, goalValue: goalInfo.goalValue)
+        let cancellable = viewModel.goalValueObservable
+            .first()
+            .sink { presentingValue in
+                if presentingValue == goalInfo.goalValue {
+                    expectedObserve.fulfill()
+                }
+            }
+
+        waitForExpectations(timeout: 1, handler: nil)
+        cancellable.cancel()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitialInput_TimeType() {
+        let expectedObserve = expectation(description: "initialInputTime")
+        let goalInfo = GoalInfo(goalType: .time, goalValue: "11:11")
+        let viewModel = GoalValueSetupViewModel(goalType: goalInfo.goalType, goalValue: goalInfo.goalValue)
+        let initialInput = "1"
+
+        let cancellable = viewModel.goalValueObservable
+            .dropFirst()
+            .sink { presentingValue in
+                if presentingValue == "00:01" {
+                    expectedObserve.fulfill()
+                }
+            }
+        viewModel.didInputNumber(initialInput)
+
+        waitForExpectations(timeout: 1, handler: nil)
+        cancellable.cancel()
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testInitialInput_DistanceType() {
+        let expectedObserve = expectation(description: "initialInputTime")
+        let goalInfo = GoalInfo(goalType: .distance, goalValue: "15.15")
+        let viewModel = GoalValueSetupViewModel(goalType: goalInfo.goalType, goalValue: goalInfo.goalValue)
+        let initialInput = "1"
+
+        let cancellable = viewModel.goalValueObservable
+            .dropFirst()
+            .sink { presentingValue in
+                if presentingValue == initialInput {
+                    expectedObserve.fulfill()
+                }
+            }
+        viewModel.didInputNumber(initialInput)
+
+        waitForExpectations(timeout: 1, handler: nil)
+        cancellable.cancel()
+    }
+
+    func testFilterInputs_DistanceType() {
+        let inputStreams = [
+            ["1", "1", "1", "1", ".", "2", ".", "2", "2", "2"], // 11.22
+            ["1", "1", "1", "1", "1", "1"], // 11
+            ["1", "1", "1", "."], // 11.
+            [".", ".", "2", "2", "2"], // .22
+            ["0", "2", ".", "1"], // 02.1
+        ]
+        let expectedResults = [
+            "11.22",
+            "11",
+            "11.",
+            ".22",
+            "02.1",
+        ]
+
+        inputStreams.enumerated().forEach { idx, inputs in
+            let goalInfo = GoalInfo(goalType: .distance, goalValue: "15.15")
+            let viewModel = GoalValueSetupViewModel(goalType: goalInfo.goalType, goalValue: goalInfo.goalValue)
+
+            let expectedObserve = expectation(description: "invalidDistanceInputs")
+            let expectedResult = expectedResults[idx]
+            let expectedNumReceived = expectedResults[idx].count
+            var numReceived = 0
+            let cancellable = viewModel.goalValueObservable
+                .dropFirst()
+                .sink { presentingValue in
+                    numReceived += 1
+                    if presentingValue == expectedResult {
+                        expectedObserve.fulfill()
+                    }
+                }
+
+            let cancellable2 = inputs.publisher.sink { viewModel.didInputNumber($0) }
+            waitForExpectations(timeout: 1, handler: nil)
+            XCTAssertEqual(expectedNumReceived, numReceived)
+            cancellable.cancel()
+            cancellable2.cancel()
         }
     }
+    
+    func testFilterInputs_TimeType() {
+        let inputStreams = [
+            ["1", "1", "1", "1", "2", "2", "2", "2"],   // 11:11
+            ["0", "0", "1", "1", "1"], // 01:11
+            ["2", "2", "2"], // 02:22
+            ["3", "3"], // 00:33
+            ["4"], // 00:04
+        ]
+        let expectedResults = [
+            "11:11",
+            "01:11",
+            "02:22",
+            "00:33",
+            "00:04",
+        ]
+        let expectedNumReceived = [4,3,3,2,1]
+        
+        inputStreams.enumerated().forEach { idx, inputs in
+            let goalInfo = GoalInfo(goalType: .time, goalValue: "")
+            let viewModel = GoalValueSetupViewModel(goalType: goalInfo.goalType, goalValue: goalInfo.goalValue)
 
+            let expectedObserve = expectation(description: "invalidTimeInputs")
+            let expectedResult = expectedResults[idx]
+            let expectedNumReceived = expectedNumReceived[idx]
+            var numReceived = 0
+            let cancellable = viewModel.goalValueObservable
+                .dropFirst()
+                .sink { presentingValue in
+                    numReceived += 1
+                    if presentingValue == expectedResult {
+                        expectedObserve.fulfill()
+                    }
+                }
+
+            let cancellable2 = inputs.publisher.sink { viewModel.didInputNumber($0) }
+            waitForExpectations(timeout: 1, handler: nil)
+            XCTAssertEqual(expectedNumReceived, numReceived)
+            cancellable.cancel()
+            cancellable2.cancel()
+        }
+    }
 }
