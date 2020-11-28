ed0bc7f4cfca44baa4c17c3114ed5994ada83e75
gihyun cho <whrlgus33@gmail.com>
2020-11-28 23:58:52 +0900

    (#35) test: (P2) PrepareRunViewModel Test 작성: didChangeGoalValue

--- BoostRunClub/ViewModels/PrepareRunViewModelTest.swift
+++ BoostRunClub/ViewModels/PrepareRunViewModelTest.swift
@@ -13,18 +13,13 @@ class PrepareRunViewModelTest: XCTestCase {
     var prepareVM: PrepareRunViewModel!
     var cancellables: Set<AnyCancellable>!
 
-    // func didTapShowProfileButton()
-    /*
-     func didTapSetGoalButton() *
-     func didTapStartButton()
-     func didChangeGoalType(_ goalType: GoalType)
-     func didChangeGoalValue(_ goalValue: String?)
-     func didTapGoalValueButton()
-     */
-
     override func setUp() {
         prepareVM = PrepareRunViewModel(locationProvider: LocationProviderMock())
         cancellables = []
+    }
+
+    override func tearDown() {
+        cancellables.removeAll()
     }
 
     // input -> output
@@ -53,30 +48,94 @@ class PrepareRunViewModelTest: XCTestCase {
         XCTAssertEqual(allCases, results)
     }
 
-    func testdidChangeGoalType() {
+    func testDidChangeGoalType_allCasesExceptNone() {
         let allCases: [GoalType] = [.distance, .speed, .time]
         var results = [GoalInfo]()
 
         allCases.forEach { goalType in
             let receivedSignal = expectation(description: "received correct goal type 장임호가 쓴 테스트")
-            let cancellable = prepareVM.goalTypeObservable.zip(prepareVM.goalValueObservable)
-                .dropFirst()
-                .sink {
-                    if $0.0 == goalType,
-                       $0.1 == goalType.initialValue
-                    {
-                        results.append(GoalInfo(goalType: $0.0, goalValue: $0.1))
-                        receivedSignal.fulfill()
-                    }
+
+            prepareVM.goalTypeObservable.send(.none)
+            let cancellable = Publishers.CombineLatest3(
+                prepareVM.goalTypeObservable,
+                prepareVM.goalValueObservable,
+                prepareVM.goalTypeSetupClosed
+            )
+            .sink {
+                print($0)
+                if $0.0 == goalType,
+                   $0.1 == goalType.initialValue
+                {
+                    results.append(GoalInfo(goalType: $0.0, goalValue: $0.1))
+                    receivedSignal.fulfill()
                 }
+            }
             prepareVM.didChangeGoalType(goalType)
             waitForExpectations(timeout: 1, handler: nil)
             cancellable.cancel()
         }
-        
+
         XCTAssertEqual(allCases.count, results.count)
         allCases.enumerated().forEach { index, goalType in
             XCTAssertEqual(GoalInfo(goalType: goalType, goalValue: goalType.initialValue), results[index])
         }
     }
+
+    func testDidChangeGoalType_noneCase() {
+        let goalType = GoalType.none
+        prepareVM.goalTypeObservable.send(.distance)
+        let receivedSignal = expectation(description: "received correct goal type(.none)")
+        let cancellable = prepareVM.goalTypeObservable.zip(prepareVM.goalValueObservable).dropFirst()
+            .sink {
+                if $0.0 == goalType,
+                   $0.1 == goalType.initialValue
+                {
+                    receivedSignal.fulfill()
+                }
+            }
+        prepareVM.didChangeGoalType(goalType)
+        waitForExpectations(timeout: 1, handler: nil)
+        cancellable.cancel()
+    }
+
+    func testDidChangeGoalValue_input_nil() {
+        let goalValue: String? = nil
+        let receivedSignal = expectation(description: "")
+
+        prepareVM.goalValueSetupClosed.sink { _ in
+            receivedSignal.fulfill()
+        }.store(in: &cancellables)
+
+        prepareVM.goalValueObservable.dropFirst().sink {
+            print($0)
+            XCTFail()
+        }.store(in: &cancellables)
+
+        prepareVM.didChangeGoalValue(goalValue)
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    func testDidChangeGoalValue_input_non_nil() {
+        let goalValue = "Goal Value"
+        let receivedSignal = expectation(description: "")
+
+        prepareVM.goalValueObservable.send(goalValue)
+        prepareVM.goalValueObservable.dropFirst()
+            .sink {
+                if $0 == goalValue {
+                    receivedSignal.fulfill()
+                }
+            }.store(in: &cancellables)
+        prepareVM.didChangeGoalValue(goalValue)
+        waitForExpectations(timeout: 1, handler: nil)
+    }
 }
+
+// func didTapShowProfileButton()
+/*
+ func didTapSetGoalButton() *
+ func didTapStartButton() *
+ func didChangeGoalType(_ goalType: GoalType) *
+ func didChangeGoalValue(_ goalValue: String?)
+ func didTapGoalValueButton()
+ */
