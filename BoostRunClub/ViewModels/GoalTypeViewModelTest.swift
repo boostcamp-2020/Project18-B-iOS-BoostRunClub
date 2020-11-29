fe6399c1b98c46e879167828cb29c990b8f354c4
Eric Jang <940709@gmail.com>
2020-11-30 07:04:01 +0900

    (#38) test: GoalTypeViewModelTest testDidSelectGoalType

--- BoostRunClub/ViewModels/GoalTypeViewModelTest.swift
+++ BoostRunClub/ViewModels/GoalTypeViewModelTest.swift
@@ -41,4 +41,30 @@ class GoalTypeViewModelTest: XCTestCase {
         waitForExpectations(timeout: 1, handler: nil)
         cancellable.cancel()
     }
+    
+    func testDidSelectGoalType() {
+        let allCases: [GoalType] = [.distance, .speed, .time]
+        
+        allCases.forEach { goalType in
+            let receivedSignal = expectation(description: "received signal that user selected goal type")
+            
+            let cancellable = Publishers.CombineLatest(
+                goalTypeVM.goalTypeObservable,
+                goalTypeVM.closeSheetSignal
+            )
+            .sink {
+                if $0 == goalType,
+                   $1 == goalType
+                {
+                    receivedSignal.fulfill()
+                } else {
+                    XCTFail("GoalType 선택 후 전송하는 값과 들어오는 값이 일치하지 않음")
+                }
+            }
+            
+            goalTypeVM.didSelectGoalType(goalType)
+            waitForExpectations(timeout: 1, handler: nil)
+            cancellable.cancel()
+        }
+    }
 }
