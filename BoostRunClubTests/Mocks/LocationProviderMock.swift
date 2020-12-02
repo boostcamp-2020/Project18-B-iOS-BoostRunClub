c0b946635abce5f3d16f20b3ed86862f52c6570b
Eric Jang <940709@gmail.com>
2020-12-02 22:48:41 +0900

    (#55) fix: LocationProviable 함수 추가

--- BoostRunClubTests/Mocks/LocationProviderMock.swift
+++ BoostRunClubTests/Mocks/LocationProviderMock.swift
@@ -10,6 +10,16 @@ import CoreLocation
 import Foundation
 
 class LocationProviderMock: LocationProvidable {
+    var isRunningBackgroundTask = false
+
+    func startBackgroundTask() {
+        isRunningBackgroundTask = true
+    }
+
+    func stopBackgroundTask() {
+        isRunningBackgroundTask = false
+    }
+
     var locationSubject = PassthroughSubject<CLLocation, Never>()
 
     func send(_ location: CLLocation) {
