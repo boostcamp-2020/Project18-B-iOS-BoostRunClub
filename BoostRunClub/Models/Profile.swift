cf8667a1f48174e0989562bf22df63e5dded4e51
Eric Jang <940709@gmail.com>
2020-12-09 17:21:40 +0900

    (#90) feat: Profile 구조체 추가

---
+++ BoostRunClub/Models/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  BoostRunClub
+//
+//  Created by Imho Jang on 2020/12/09.
+//
+
+import Foundation
+
+struct Profile {
+    var image: Data?
+    var name: String
+    var hometown: String
+    var bio: String
+}
