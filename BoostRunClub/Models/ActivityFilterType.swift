fa45d7783e2d3aade71ee269f59eea15c3ff6fe0
SHINVVOO <sinsdhk@gmail.com>
2020-12-10 12:21:44 +0900

    (#117) style: 불필요 주석 제거

--- BoostRunClub/Models/ActivityFilterType.swift
+++ BoostRunClub/Models/ActivityFilterType.swift
@@ -39,31 +39,6 @@ enum ActivityFilterType: Int {
             }
     }
 
-//    func groupDateRanges(from dates: [Date]) -> [DateRange] {
-//        guard !dates.isEmpty else { return [] }
-//
-//        if self == .all {
-//            return [DateRange(start: dates.first!, end: dates.last ?? dates.first!)]
-//        }
-//
-//        var results = [DateRange]()
-//        dates.forEach {
-//            if
-//                results.isEmpty,
-//                let range = $0.rangeOf(type: self)
-//            {
-//                results.append(range)
-//            } else if
-//                let lastRange = results.last,
-//                !lastRange.contains(date: $0),
-//                let newRange = $0.rangeOf(type: self)
-//            {
-//                results.append(newRange)
-//            }
-//        }
-//        return results
-//    }
-
     func rangeDescription(from range: DateRange) -> String {
         switch self {
         case .week:
