5e0fd4827bc0a57ecc6dc6315beb69af4d6fe9d6
Eric Jang <940709@gmail.com>
2020-12-19 23:26:30 +0900

    (#189) style: 다크모드 컬러셋 추가 및 적용

--- BoostRunClub/Views/CircleButton.swift
+++ BoostRunClub/Views/CircleButton.swift
@@ -23,25 +23,30 @@ class CircleButton: UIButton {
     }
 
     private func commonInit(with buttonStyle: Style = .start) {
+        setTitleColor(.label, for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        imageView?.contentMode = .scaleAspectFill
+
         switch buttonStyle {
         case .start:
-            backgroundColor = #colorLiteral(red: 0.9763557315, green: 0.9324046969, blue: 0, alpha: 1)
+            backgroundColor = UIColor(named: "accent")
             setTitle("시작", for: .normal)
+            setTitleColor(UIColor(named: "accent2"), for: .normal)
         case .stop:
             setSFSymbol(iconName: "stop.fill", size: 25, tintColor: .systemBackground, backgroundColor: .label)
         case .pause:
             setSFSymbol(iconName: "pause.fill", size: 25, tintColor: .systemBackground, backgroundColor: .label)
+            setTitleColor(UIColor(named: "accent2"), for: .normal)
         case .resume:
-            setSFSymbol(iconName: "play.fill", size: 25, tintColor: .label, backgroundColor: #colorLiteral(red: 0.9763557315, green: 0.9324046969, blue: 0, alpha: 1))
+            setSFSymbol(iconName: "play.fill",
+                        size: 25,
+                        tintColor: .label,
+                        backgroundColor: UIColor(named: "brcYellow")!)
         case .locate:
             setSFSymbol(iconName: "location.fill", size: 25, tintColor: .systemBackground, backgroundColor: .label)
         case .exit:
             setSFSymbol(iconName: "xmark", size: 25, tintColor: .systemBackground, backgroundColor: .label)
         }
-
-        setTitleColor(.label, for: .normal)
-        titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
-        imageView?.contentMode = .scaleAspectFill
     }
 
     override func layoutSubviews() {
